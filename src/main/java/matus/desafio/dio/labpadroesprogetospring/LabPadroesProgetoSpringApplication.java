bdc338a598a2b15bcf1b5f3c8967e2a04e1e9b55
MatusMoura <matus.barreto@gmail.com>
2022-10-13 21:29:04 -0300

    Upload project

---
+++ src/main/java/matus/desafio/dio/labpadroesprogetospring/LabPadroesProgetoSpringApplication.java
@@ -0,0 +1,13 @@
+package matus.desafio.dio.labpadroesprogetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProgetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProgetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matus/desafio/dio/labpadroesprogetospring/LabPadroesProgetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package matus.desafio.dio.labpadroesprogetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProgetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
